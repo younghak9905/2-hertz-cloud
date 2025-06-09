0b3860cae211721bd0345a04489e550c470f7168
younghak9905 <yeonghak9905@gmail.com>
2025-06-09 11:20:41 +0900

    refactor: nat 서브넷 zone 분리

--- terraform/gcp/environments/prod/main.tf
+++ terraform/gcp/environments/prod/main.tf
@@ -63,7 +63,7 @@ resource "google_compute_router_nat" "nat" {
 }
 
 locals {
-  nat_subnet_info = data.terraform_remote_state.shared.outputs.nat_subnet_info
+  nat_subnet_info = data.terraform_remote_state.shared.outputs.nat_b_subnet_info
   firewall_rules  = data.terraform_remote_state.shared.outputs.firewall_rules
 
   region           = var.region
