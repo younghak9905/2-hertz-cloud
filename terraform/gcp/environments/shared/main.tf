f1c32f42a266912228f38722cc0bc757149cbf31
ddongu <ehdrb1205@naver.com>
2025-06-19 17:55:47 +0900

    refactor: 사용하지 않는 방화벽 삭제(websocket, 중복)

--- terraform/gcp/environments/shared/main.tf
+++ terraform/gcp/environments/shared/main.tf
@@ -204,7 +204,7 @@ locals {
     target_tags   = ["allow-vpn-ssh"]
     description   = "Allow SSH from VPN clients"
     },
-     {
+    {
       name          = "${var.vpc_name}-fw-lb-to-frontend"
       direction     = "INGRESS"
       priority      = 1000
@@ -217,22 +217,21 @@ locals {
       protocol      = "tcp"
       ports         = ["80"]
     },
-
-    # (4) GCP 헬스체크 → Frontend VM(포트 80) 허용
+    # # (4) GCP 헬스체크 → Frontend VM(포트 80) 허용
+    # {
+    #   name          = "${var.vpc_name}-fw-healthcheck-to-frontend"
+    #   direction     = "INGRESS"
+    #   priority      = 1000
+    #   description   = "Allow GCP Health Checks (130.211.0.0/22, 35.191.0.0/16) to Frontend on TCP:80"
+    #   source_ranges = [
+    #     "130.211.0.0/22",
+    #     "35.191.0.0/16",
+    #   ]
+    #   target_tags   = ["frontend"]
+    #   protocol      = "tcp"
+    #   ports         = ["80"]
+    # },
     {
-      name          = "${var.vpc_name}-fw-healthcheck-to-frontend"
-      direction     = "INGRESS"
-      priority      = 1000
-      description   = "Allow GCP Health Checks (130.211.0.0/22, 35.191.0.0/16) to Frontend on TCP:80"
-      source_ranges = [
-        "130.211.0.0/22",
-        "35.191.0.0/16",
-      ]
-      target_tags   = ["frontend"]
-      protocol      = "tcp"
-      ports         = ["80"]
-    },
-     {
       name          = "${var.vpc_name}-fw-lb-to-backend"
       direction     = "INGRESS"
       priority      = 1000
@@ -245,19 +244,19 @@ locals {
       protocol      = "tcp"
       ports         = ["8080"]
     },
-    {
-      name          = "${var.vpc_name}-fw-healthcheck-to-backend"
-      direction     = "INGRESS"
-      priority      = 1000
-      description   = "Allow GCP Health Checks (130.211.0.0/22, 35.191.0.0/16) to Backend on TCP:8080"
-      source_ranges = [
-        "130.211.0.0/22",
-        "35.191.0.0/16",
-      ]
-      target_tags   = ["websocket"]
-      protocol      = "tcp"
-      ports         = ["9093"]
-    }
+    # {
+    #   name          = "${var.vpc_name}-fw-healthcheck-to-backend"
+    #   direction     = "INGRESS"
+    #   priority      = 1000
+    #   description   = "Allow GCP Health Checks (130.211.0.0/22, 35.191.0.0/16) to Backend on TCP:8080"
+    #   source_ranges = [
+    #     "130.211.0.0/22",
+    #     "35.191.0.0/16",
+    #   ]
+    #   target_tags   = ["websocket"]
+    #   protocol      = "tcp"
+    #   ports         = ["9093"]
+    # }
   ]
 }
 
